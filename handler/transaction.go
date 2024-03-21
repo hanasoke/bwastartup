e172d99ebc310e461da0491d3284712c37e96648
hanasoke@gmail.com <hanasoke@gmail.com>
2024-03-21 10:47:53 +0700

    add cors

--- handler/transaction.go
+++ handler/transaction.go
@@ -2,7 +2,6 @@ package handler
 
 import (
 	"bwastartup/helper"
-	"bwastartup/payment"
 	"bwastartup/transaction"
 	"bwastartup/user"
 	"net/http"
@@ -12,11 +11,10 @@ import (
 
 type transactionHandler struct {
 	service 		transaction.Service
-	paymentService 	payment.Service
 }
 
-func NewTransactionHandler(service transaction.Service, paymentService payment.Service) *transactionHandler {
-	return &transactionHandler{service, paymentService}
+func NewTransactionHandler(service transaction.Service) *transactionHandler {
+	return &transactionHandler{service}
 }
 
 func (h *transactionHandler) GetCampaignTransactions(c *gin.Context) {
@@ -103,7 +101,7 @@ func (h *transactionHandler) GetNotification(c *gin.Context)  {
 		return
 	}
 
-	err = h.paymentService.ProcessPayment(input)
+	err = h.service.ProcessPayment(input)
 	if err != nil {
 		response := helper.APIResponse("Failed to process notification", http.StatusBadRequest, "error", nil)
 		c.JSON(http.StatusBadRequest, response)
